86223a8aa4f7483be1da88aaddb4525fec7cfb40
Jie <chenjieva@gmail.com>
2022-03-24 22:12:16 -0400

    Added the Color Extension

--- Assets/Example/Scripts/TexturePreviewExample.cs
+++ Assets/Example/Scripts/TexturePreviewExample.cs
@@ -16,16 +16,22 @@ namespace TinyTools.Examples {
         [SerializeField]
         public int TestInt;
 
+        [SerializeField]
+        public Color TestColor;
         // Start is called before the first frame update
         void Start()
         {
+            Color color = new Color(69, 12, 65);
+            //int r = (int)color.r, g = (int)color.g, b = (int)color.b;
+            //string _color = r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+            Debug.Log("The Color is :" + color.RgbToHex());
 
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            //Debug.Log("The Hex for the color is: " + TestColor.RgbToHex());
         }
     }
 }
